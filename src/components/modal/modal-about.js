0d37c352d9c7d35e127daabcbceb278b19a45d84
kajaarielle <47561841+kajaarielle@users.noreply.github.com>
2024-11-10 13:14:56 +0100

    credits

--- src/components/modal/modal-about.js
+++ src/components/modal/modal-about.js
@@ -12,6 +12,7 @@ export default function ModalAbout({modalHandler}) {
             <p>Application version: {appVersion}</p>
             <p>This Sudoku web application was created for <a href="https://sudokuexchange.com/"
             >SudokuExchange.com</a> by <a href="https://grantm.github.io/">Grant McLean</a>.</p>
+            <p>It has been altered on this webpage by Kaja Bergseth.</p>
             <p>It is <a href="https://www.fsf.org/about/what-is-free-software">free software</a>{' '}
             which you can use, copy, modify and share under the terms of the
             GNU Affero General Public License version 3 (<a href="https://opensource.org/licenses/AGPL-3.0"
